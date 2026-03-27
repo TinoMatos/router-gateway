d794a6b0b2ca1063911757a71b00c28b8c69a19b
Tino Matos <tinobelmont@gmail.com>
2026-03-27 13:48:34 -0300

    router gatway

---
+++ tests/router.e2e.test.ts
@@ -0,0 +1,65 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import { createServer } from '../src/server.ts'
+import { config } from '../src/config.ts'
+import { type LLMResponse, OpenRouterService } from '../src/openrouterService.ts'
+
+console.assert(
+    process.env.OPENROUTER_API_KEY,
+    'OPENROUTER_API_KEY is not set in env variables'
+)
+
+
+test('routes to cheapest model by default', async () => {
+    const customConfig ={
+        ...config,
+        provider: {
+            ...config.provider,
+            sort: {
+                ...config.provider.sort,
+                by: 'price'
+            }
+        }
+    }
+    const routerService = new OpenRouterService(customConfig)
+    const app = createServer(routerService)
+
+    const response = await app.inject({
+        method: 'POST',
+        url: '/chat',
+        body:{ question:'What is rate limiting?'}
+    })
+    assert.equal(response.statusCode, 200)
+    const body = response.json() as LLMResponse
+
+    assert.ok(body.model, 'should return a model name')
+    assert.ok(body.content, 'should return content')
+    console.log('[price] model used:', body.model)
+})
+
+test('routes to highest throughput model by default', async () => {
+    const customConfig ={
+        ...config,
+        provider: {
+            ...config.provider,
+            sort: {
+                ...config.provider.sort,
+                by: 'throughput'
+            }
+        }
+    }
+    const routerService = new OpenRouterService(customConfig)
+    const app = createServer(routerService)
+
+    const response = await app.inject({
+        method: 'POST',
+        url: '/chat',
+        body:{ question:'What is rate limiting?'}
+    })
+    assert.equal(response.statusCode, 200)
+    const body = response.json() as LLMResponse
+
+    assert.ok(body.model, 'should return a model name')
+    assert.ok(body.content, 'should return content')
+    console.log('[throughput] model used:', body.model)
+})
